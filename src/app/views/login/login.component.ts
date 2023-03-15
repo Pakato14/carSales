4b767aa1a9e8d73c3c707f56b511ab86141099fa
Daniel Araujo <daniel.araujo@sedet.ce.gov.br>
2023-03-14 23:21:12 -0300

    sweetalert2

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,3 +1,4 @@
+import { AlertService } from './../../resources/services/alert.service';
 import { LoginService } from './../../resources/services/login.service';
 import { RequestLogin } from './../../resources/models/RequestLogin';
 import { Component, OnInit } from '@angular/core';
@@ -10,7 +11,8 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
   public requestLogin!: RequestLogin
 
-  constructor(private loginService: LoginService) { }
+  constructor(private loginService: LoginService,
+    private alertService:AlertService) { }
 
   ngOnInit(): void {
     this.requestLogin = new RequestLogin
@@ -19,10 +21,10 @@ export class LoginComponent implements OnInit {
   public doLogin(): void{
     this.loginService.doLogin(this.requestLogin).subscribe(
       (data) => {
-        console.log(data)
+        this.alertService.info('Funcionalidade ainda não implementada!', 'Atenção')
       },
-      (error) =>{
-        console.log(error)
+      (httpError) =>{
+        this.alertService.error(httpError.error.message, httpError.error.title)
       }
     )
   }
